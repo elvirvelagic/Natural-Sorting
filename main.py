69fed87b5c326d06afdc29bcbb572c85d700f56c
elvirvelagic <elvir.1998@hotmail.com>
2024-03-15 08:19:28 +0100

    Natural sorting

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-test
+import os
+from natsort import natsorted, realsorted, humansorted, os_sorted
+
+list1 = ['5', '6', '1', '11', '16', '6']
+
+print('sort', '\n',sorted(list1))
+print('natsort', '\n',natsorted(list1))
+print('*' * 100)
+
+list2 = ['4 ft 2 in', '6 ft 2 in', '1 ft 7 in', '11 ft 8 in', '16 ft 2 in']
+
+print('sort', '\n',sorted(list2))
+print('natsort', '\n',natsorted(list2))
+print('*' * 100)
+
+
+list3 = ['version-1.2.3', 'version-2.4', 'version-11.2.8', 'version-12.2', 'version-4.5']
+
+print('sort', '\n',sorted(list3))
+print('natsort', '\n', natsorted(list3))
+print('*' * 100)
+
+list4 = ['1.2', '-3', 2, 2.8, -1, '-3.1']
+
+print('natsort', '\n',natsorted(list4))
+print('realsort', '\n', realsorted(list4))
+print('*' * 100)
+
+list5 = ['banana', 'Band', 'book', 'apple', 'clown', 'Computer']
+
+print('natsort', '\n',natsorted(list5))
+print('humansort', '\n',humansorted(list5))
+
+print(os.listdir('/'))
+print(os_sorted(os.listdir('/')))
